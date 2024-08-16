c3144f398f41fbb792eb8a048b719b33636ff299
rohitchauhan2004 <rohit.22210640@viit.ac.in>
2024-08-16 23:19:13 +0530

    Add files via upload
    This is my first website i have made as the first task of webdevelopment internship

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to handle the scroll event and change the navbar background color
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
